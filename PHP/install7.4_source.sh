fc2b31ed4881adcbb1f06c775edae73d612c7487
Kenshin <517701884@qq.com>
2020-03-11 17:32:55 +0800

    php7.4

--- PHP/install7.4_source.sh
+++ PHP/install7.4_source.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+wget https://www.php.net/distributions/php-7.4.3.tar.bz2
+tar -xjf php-7.4.3.tar.bz2
+cd php-7.4.3
+
 apt install libxml2-dev -y
 apt install libbz2-dev -y
 apt install libsqlite3-dev -y
@@ -13,9 +17,6 @@ apt install libonig-dev -y
 
 
 ./configure --prefix=/usr/local/php7 --with-config-file-scan-dir=/usr/local/php7/etc/php.d --with-config-file-path=/usr/local/php7/etc --enable-mbstring --enable-zip --enable-bcmath --enable-pcntl --enable-ftp --enable-xml --enable-shmop --enable-soap --enable-intl --with-openssl --enable-exif --enable-calendar --enable-sysvmsg --enable-sysvsem --enable-sysvshm --enable-opcache --enable-fpm --enable-session --enable-sockets --enable-mbregex --enable-wddx --with-curl --with-iconv --with-gd --with-jpeg-dir=/usr --with-png-dir=/usr --with-zlib-dir=/usr --with-freetype-dir=/usr --enable-gd-jis-conv --with-openssl --with-pdo-mysql=mysqlnd --with-gettext=/usr --with-zlib=/usr --with-bz2=/usr --with-recode=/usr --with-xmlrpc --with-mysqli=mysqlnd
-
-
-
 
 
 make install
