23c616380e6da0414cac2efae7a83b45d41d3160
kenshin <dx0w0xb@outlook.com>
2020-06-05 18:26:07 +0800

    mysql

--- mysql/reinstall_mysql.sh
+++ mysql/reinstall_mysql.sh
@@ -11,6 +11,7 @@ mysql
 update mysql.user set authentication_string=PASSWORD('root'),plugin='mysql_native_password' where user='root';
 flush privileges;
 
+
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 exit;
@@ -23,3 +24,11 @@ skip-name-resolve
 
 
 service mysql restart
+
+# 8.0改密码
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+# 8.0后创建用户和赋给权限
+create user 'Gary'@'%' identified by 'Gary@2019'; 
+grant all privileges on *.* to 'Gary'@'%'
+
