0cccf0587726ab5108f26ddda78b5aebbe29e3db
kenshin <dx0w0xb@outlook.com>
2021-03-22 17:33:41 +0800

    snapshot

--- vbox/importopt.sh
+++ vbox/importopt.sh
@@ -14,13 +14,23 @@ while [[ $# -ge 1 ]]; do
         c=$2
         shift 2
         ;;
+    -m | --memory)
+        m=$2
+        shift 2
+        ;;
+    -C | --cpus)
+        C=$2
+        shift 2
+        ;;
     -h | --help)
         h="h"
         echo "
 -i | --image: image path
 -n | --name: vmname
 -c | --netcard: network card name
-./importopt.sh -i ./u18_04_4_500g_root_vbox_plus.ova -n test -c em1
+-m | --memory: memory size (MB) default 2048
+-C | --cpus: cpu number default 1
+./importopt.sh -i ./u18_04_4_500g_root_vbox_plus.ova -n test -c em1 -m 2048 -C 1
             "
         shift 1
         exit
@@ -45,8 +55,19 @@ done
 path=$i
 vmname=$n
 ncard=$c
+memory=$m
+cpus=$C
 
-VBoxManage import "$path" --vsys 0 --vmname "$vmname"
+if [ ! $memory ]; then
+    memory=2048;
+fi
+
+if [ ! $cpus ]; then
+    cpus=1;
+fi
+
+
+VBoxManage import "$path" --vsys 0 --vmname "$vmname" --memory $memory --cpus $cpus
 
 VBoxManage modifyvm "$vmname" --nic1 bridged
 VBoxManage modifyvm "$vmname" --bridgeadapter1 "$ncard"
---
+++ vbox/snapshot.sh
@@ -0,0 +1,4 @@
+ssh root@192.168.2.139 VBoxManage snapshot bd3 take yq`date "+%y%m%d%H%M%S"`;
+ssh root@192.168.2.140 VBoxManage snapshot bd8 take yq`date "+%y%m%d%H%M%S"`;
+ssh root@192.168.2.29 VBoxManage snapshot bdt2 take yq`date "+%y%m%d%H%M%S"`;VBoxManage snapshot bd7 take yq`date "+%y%m%d%H%M%S"`;
+ssh root@192.168.2.28 VBoxManage snapshot bd6 take yq`date "+%y%m%d%H%M%S"`;
